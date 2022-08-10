4dd3ab2ced894df7eea9da41cb7772e24e7c49d4
Alex <alexsa-8@mail.ru>
2022-08-10 14:18:25 +0300

    исправление

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,40 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
         // Задание 1
         System.out.println("\nЗадание 1");
         int i=0;
-        while (i<=9){
-            i++;
-            System.out.print(i);
-        }
-        System.out.print("\n");
-        for (;i>=1;){
-            System.out.print(i);
-            i--;
-        }
+        while (i<=9){             // while (i<10) {
+            i++;                  // не надо
+            System.out.print(i);  //   System.out.print(++i + " ");
+        }                         // }
+        System.out.print("\n");   // System.out.println();
+
+        for (;i>=1;){             // for (;i>0;i--){
+            System.out.print(i);  //   System.out.print(i+ " ");
+            i--;                  // не надо
+        }                         // }
 
         // Задание 2
         System.out.println("\nЗадание 2");
-        int friday=1;
-        for (; friday<=31; friday++) {
-            if (friday%7==0){
-                System.out.println("Сегодня пятница, "+friday+"-е число. Необходимо подготовить отчет.");
-            }
+        int friday=5;
+        for (int currentFriday=friday; currentFriday <= 31; currentFriday +=7) {
+            System.out.println("Сегодня пятница, "+currentFriday+"-е число. Необходимо подготовить отчет.");
         }
 
 
         // Задание 3
         System.out.println("\nЗадание 3");
-        int period=100;
-        for (int years=0; years<=2122; years+=79) {
-            if (years>=1822){
-                System.out.println(years);
+        int currentYear= LocalDate.now().getYear(); // выдаёт текущий год (2022)
+
+        int start=currentYear - 200;
+        int end=currentYear + 100;
+
+        for (int year=start; year<=end; year++) {
+            if (year%79==0){
+                System.out.println(year);
             }
         }
     }
