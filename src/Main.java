324f82c42a9a79ca252764ef452bdeff73ba72d1
Alex <alexsa-8@mail.ru>
2022-08-10 13:04:49 +0300

    сделано 3 задания

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        // Задание 1
+        System.out.println("\nЗадание 1");
+        int i=0;
+        while (i<=9){
+            i++;
+            System.out.print(i);
+        }
+        System.out.print("\n");
+        for (;i>=1;){
+            System.out.print(i);
+            i--;
+        }
+
+        // Задание 2
+        System.out.println("\nЗадание 2");
+        int friday=1;
+        for (; friday<=31; friday++) {
+            if (friday%7==0){
+                System.out.println("Сегодня пятница, "+friday+"-е число. Необходимо подготовить отчет.");
+            }
+        }
+
+
+        // Задание 3
+        System.out.println("\nЗадание 3");
+        int period=100;
+        for (int years=0; years<=2122; years+=79) {
+            if (years>=1822){
+                System.out.println(years);
+            }
+        }
     }
 }
